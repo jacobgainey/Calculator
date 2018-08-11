9adc7fd0bf3051051738ba45ea2174ce6bca951a
Jacob Noonan Gainey <jgainey@carolina.rr.com>
2018-08-11 13:44:11 -0400

    Update

--- AppDelegate.cs
+++ AppDelegate.cs
@@ -3,7 +3,7 @@ using UIKit;
 
 namespace Calculator
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
     // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
     [Register("AppDelegate")]
     public class AppDelegate : UIApplicationDelegate
